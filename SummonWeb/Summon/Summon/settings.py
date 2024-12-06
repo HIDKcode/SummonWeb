e9d175f53264f15d4e862094f8b0d18d0a1e0356
HIDKcode <hidkprogramador@gmail.com>
2024-12-06 06:15:38 -0300

    Avance 1

--- SummonWeb/Summon/Summon/settings.py
+++ SummonWeb/Summon/Summon/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'summonapp'
+    'summonapp',
     'django_user_agents',
 ]
 
